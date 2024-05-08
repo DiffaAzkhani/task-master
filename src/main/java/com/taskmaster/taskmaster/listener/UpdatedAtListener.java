632eaa400e2eb6e963a5fb57953f7f615ed11a84
Diffa Azkhani <diffaazkhani1@gmail.com>
2024-05-08 14:33:27 +0700

    add: date time listener

---
+++ src/main/java/com/taskmaster/taskmaster/listener/UpdatedAtListener.java
@@ -0,0 +1,20 @@
+package com.taskmaster.taskmaster.listener;
+
+import com.taskmaster.taskmaster.Util.TimeUtil;
+import com.taskmaster.taskmaster.event.UpdatedAtAware;
+
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.time.LocalDateTime;
+
+public class UpdatedAtListener {
+    @PrePersist
+    @PreUpdate
+    public void setUpdatedAt(Object entity){
+        LocalDateTime localDateTime = TimeUtil.getFormattedLocalDateTimeNow();
+        if (entity instanceof UpdatedAtAware){
+            UpdatedAtAware updatedAtAware = (UpdatedAtAware) entity;
+            updatedAtAware.setUpdatedAt(localDateTime);
+        }
+    }
+}
