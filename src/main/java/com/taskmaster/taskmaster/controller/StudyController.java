b098e158958f971a518a9963e59987daa87f7d7c
Diffa Azkhani <diffaazkhani1@gmail.com>
2024-05-13 14:10:24 +0700

    add: feature get all study data with pagination

--- src/main/java/com/taskmaster/taskmaster/controller/StudyController.java
+++ src/main/java/com/taskmaster/taskmaster/controller/StudyController.java
@@ -1,10 +1,12 @@
 package com.taskmaster.taskmaster.controller;
 
 import com.taskmaster.taskmaster.model.request.AddStudyRequest;
+import com.taskmaster.taskmaster.model.response.PagingResponse;
 import com.taskmaster.taskmaster.model.response.StudyResponse;
 import com.taskmaster.taskmaster.model.response.WebResponse;
 import com.taskmaster.taskmaster.service.StudyService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +15,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -49,6 +54,28 @@ public class StudyController {
                 .message(HttpStatus.OK.getReasonPhrase())
                 .data(studyResponse)
                 .build();
+    }
+
+    @GetMapping(
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<List<StudyResponse>> getAllStudy(
+        @RequestParam(name = "page", defaultValue = "0") int page,
+        @RequestParam(name = "size", defaultValue = "10") int size
+    ) {
+        Page<StudyResponse> studyResponsePage = studyService.getAllStudyMaterial(page, size);
+        List<StudyResponse> studyResponses = studyResponsePage.getContent();
+
+        return WebResponse.<List<StudyResponse>>builder()
+            .code(HttpStatus.OK.value())
+            .message(HttpStatus.OK.getReasonPhrase())
+            .data(studyResponses)
+            .paging(PagingResponse.builder()
+                .currentPage(page)
+                .totalPage(studyResponsePage.getTotalPages())
+                .size(size)
+                .build())
+            .build();
     }
 
 }
--- src/main/java/com/taskmaster/taskmaster/service/StudyService.java
+++ src/main/java/com/taskmaster/taskmaster/service/StudyService.java
@@ -2,10 +2,13 @@ package com.taskmaster.taskmaster.service;
 
 import com.taskmaster.taskmaster.model.request.AddStudyRequest;
 import com.taskmaster.taskmaster.model.response.StudyResponse;
+import org.springframework.data.domain.Page;
 
 public interface StudyService {
     StudyResponse addStudy(AddStudyRequest request);
 
     StudyResponse getStudy(String code);
+
+    Page<StudyResponse> getAllStudyMaterial(int page, int size);
 
 }
