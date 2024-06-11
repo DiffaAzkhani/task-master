1f26b5a78eadcdc34aa692a2cb6f84a27326f3ac
Diffa Azkhani <diffaazkhani1@gmail.com>
2024-06-11 09:59:07 +0700

    add: feature add question and answer

---
+++ src/main/java/com/taskmaster/taskmaster/model/request/AddAnswerRequest.java
@@ -0,0 +1,23 @@
+package com.taskmaster.taskmaster.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AddAnswerRequest {
+
+    @NotNull
+    String answerText;
+
+    @NotBlank
+    Boolean isCorrect;
+
+}
