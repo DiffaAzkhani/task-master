a2c7ac98cb70a55b1bf05749a5f7da290c44e693
Diffa Azkhani <diffaazkhani1@gmail.com>
2024-07-26 21:02:09 +0700

    add: feature answer submission

---
+++ src/main/java/com/taskmaster/taskmaster/entity/UserAnswer.java
@@ -0,0 +1,44 @@
+package com.taskmaster.taskmaster.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "user_answer")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserAnswer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "answer_id", nullable = false)
+    private Answer answer;
+
+    private LocalDateTime answeredAt;
+
+}
