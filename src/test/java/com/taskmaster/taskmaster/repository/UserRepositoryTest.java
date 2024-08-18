9bb2f548f14d4ac62f7e39bb5adc7db99f3f8567
Diffa Azkhani <diffaazkhani1@gmail.com>
2024-08-18 12:29:28 +0700

    test: implement unit test in user repository

---
+++ src/test/java/com/taskmaster/taskmaster/repository/UserRepositoryTest.java
@@ -0,0 +1,171 @@
+package com.taskmaster.taskmaster.repository;
+
+import com.taskmaster.taskmaster.entity.Study;
+import com.taskmaster.taskmaster.entity.User;
+import com.taskmaster.taskmaster.enums.StudyCategory;
+import com.taskmaster.taskmaster.enums.StudyLevel;
+import com.taskmaster.taskmaster.enums.StudyType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DataJpaTest
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private StudyRepository studyRepository;
+
+    private User testUser;
+
+    private Study testStudy;
+
+    @BeforeEach
+    public void setUp() {
+        Set<Study> studySet = new HashSet<>();
+
+        testStudy = Study.builder()
+            .code("MATH.12.12345")
+            .name("Test Study")
+            .price(100000)
+            .discount(0)
+            .link("test-link.com")
+            .category(StudyCategory.MATHEMATICS)
+            .type(StudyType.PREMIUM)
+            .level(StudyLevel.GRADE_12)
+            .build();
+
+        studySet.add(testStudy);
+
+        testUser = User.builder()
+            .username("TestUser")
+            .password("testuser123")
+            .firstName("test")
+            .lastName("user")
+            .email("test.email@example.com")
+            .phone("12345")
+            .studies(studySet)
+            .build();
+
+        userRepository.save(testUser);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        userRepository.delete(testUser);
+        studyRepository.delete(testStudy);
+    }
+
+    @Test
+    public void givenUser_whenCheckingUsernameAndEmail_thenReturnTrueResponse() {
+        boolean existsUser = userRepository.existsByUsernameAndEmail(testUser.getUsername(), testUser.getEmail());
+        assertTrue(existsUser);
+    }
+
+    @Test
+    public void givenUser_whenCheckingUsernameAndEmail_thenReturnFalseResponse() {
+        boolean existsUser = userRepository.existsByUsernameAndEmail("Wrong Username", "wrong.email@example.com");
+        assertFalse(existsUser);
+    }
+
+    @Test
+    public void givenUser_whenFindByUsernameOrEmail_thenUserIsFound() {
+        User foundUser = userRepository.findByUsernameOrEmail(testUser.getUsername(), testUser.getEmail())
+            .orElse(null);
+
+        assertNotNull(foundUser);
+        assertEquals(testUser, foundUser);
+    }
+
+    @Test
+    public void givenUser_whenFindByUsernameOrEmail_thenUserIsNotFound() {
+        User foundUser = userRepository.findByUsernameOrEmail("Wrong Username", "wrong.email@example.com")
+            .orElse(null);
+
+        assertNull(foundUser);
+    }
+
+    @Test
+    public void givenUser_whenFindByUsername_thenUserIsFound() {
+        User foundUser = userRepository.findByUsername(testUser.getUsername())
+            .orElse(null);
+
+        assertNotNull(foundUser);
+        assertEquals(testUser, foundUser);
+    }
+
+    @Test
+    public void givenUser_whenFindByUsername_thenUserIsNotFound() {
+        User foundUser = userRepository.findByUsername("Wrong Username")
+            .orElse(null);
+
+        assertNull(foundUser);
+    }
+
+    @Test
+    public void givenUser_whenCheckingEmail_thenReturnTrueResponse() {
+        boolean existsUser = userRepository.existsByEmail(testUser.getEmail());
+        assertTrue(existsUser);
+    }
+
+    @Test
+    public void givenUser_whenCheckingEmail_thenReturnFalseResponse() {
+        boolean existsUser = userRepository.existsByEmail("wrong.email@example.com");
+        assertFalse(existsUser);
+    }
+
+    @Test
+    public void givenUser_whenFindByEmail_thenUserIsFound() {
+        User foundUser = userRepository.findByEmail(testUser.getEmail())
+            .orElse(null);
+
+        assertNotNull(foundUser);
+        assertEquals(testUser, foundUser);
+    }
+
+    @Test
+    public void givenUser_whenFindByEmail_thenUserIsNotFound() {
+        User foundUser = userRepository.findByEmail("wrong.username@example.com")
+            .orElse(null);
+
+        assertNull(foundUser);
+    }
+
+    @Test
+    public void givenUsernameAndStudies_whenCheckingUsernameAndStudies_thenReturnTrueResponse() {
+        boolean existsUser = userRepository.existsByUsernameAndStudies(testUser.getUsername(), testStudy);
+        assertTrue(existsUser);
+    }
+
+    @Test
+    public void givenUsernameAndStudies_whenCheckingUsernameAndStudies_thenReturnFalseResponse() {
+        boolean existsUser = userRepository.existsByUsernameAndStudies("Wrong username" ,testStudy);
+        assertFalse(existsUser);
+    }
+
+    @Test
+    public void givenUserPhone_whenCheckingPhone_thenReturnTrueResponse() {
+        boolean existsUser = userRepository.existsByPhone(testUser.getPhone());
+        assertTrue(existsUser);
+    }
+
+    @Test
+    public void givenUserPhone_whenCheckingPhone_thenReturnFalseResponse() {
+        boolean existsUser = userRepository.existsByEmail("54321");
+        assertFalse(existsUser);
+    }
+
+}
